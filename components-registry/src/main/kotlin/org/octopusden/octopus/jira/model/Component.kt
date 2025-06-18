96bc93a7b5f082b6ca488e9abf5efa33f29c14e6
Roman Ivanov <ivanov.roman.alexeevich@gmail.com>
2025-06-18 11:36:22 +0300

    Support system and clientCode fields of component (#40)

--- components-registry/src/main/kotlin/org/octopusden/octopus/jira/model/Component.kt
+++ components-registry/src/main/kotlin/org/octopusden/octopus/jira/model/Component.kt
@@ -2,9 +2,11 @@ package org.octopusden.octopus.jira.model
 
 data class Component(
     val id: String,
+    val system: Set<String>,
+    val clientCode: String?,
     val name: String?,
     val componentOwner: String? = null,
     val releaseManager: String? = null,
     val distribution: Distribution? = null,
-    val releasesInDefaultBranch: Boolean?,
+    val releasesInDefaultBranch: Boolean?
 )
